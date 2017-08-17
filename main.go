9114922d23a2032b99d2a87d78dcd4dfc4fa7167
josebarn <joseph.barnett@cloudlock.com>
2017-08-17 10:53:56 -0300

    first commit - simple service implementation v 1.0

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	. "github.com/josebarn/drone-tutorial/api"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	var redis_url string
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "redis, r",
+			Usage:       "redis server uri",
+			Destination: &redis_url,
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if len(redis_url) <= 0 {
+			log.Println("not using redis server")
+		}
+		ApiInit()
+		return nil
+	}
+
+	app.HideVersion = true
+
+	app.Run(os.Args)
+}
